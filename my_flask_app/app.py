c963de9c998ccd1285e12891cb81e258eaf62f02
max snyder <max@3ddd.com>
2024-07-12 10:34:01 -0400

    debug

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -55,5 +55,8 @@ def index():
     return render_template('index.html', prediction=None, probabilities=None, image_path=None)
 
 if __name__ == '__main__':
+    app.run(debug=True)
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
